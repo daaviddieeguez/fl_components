23401f8039c564bb0918dcf92699fc316b480f74
daaviddieeguez <daaviddieeguez@gmail.com>
2025-10-31 09:46:06 +0100

    OnGenerateRoutes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:fl_components/screens/alert_screen.dart';
-import 'package:fl_components/screens/card_screen.dart';
-import 'package:fl_components/screens/home_screen.dart';
-import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,13 +12,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,  
       title: 'Material App',
-      home:ListView2Screen(),
+      //home:ListView2Screen(),
+      initialRoute: 'home',
       routes: {
         'listview1': (BuildContext context) => ListView1Screen(),
         'listview2': (BuildContext context) => ListView2Screen(),
         'card': (BuildContext context) => CardScreen(),
         'alert': (BuildContext context) => AlertScreen(),
         'home': (BuildContext context) => HomeScreen()
+      },
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => AlertScreen(), 
+        );
       },
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,31 @@
+import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
+
+  final options = const['Targaryen', 'Stark', 'Lannister', 'Baratheon'];
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes de Flutter'),
+        elevation: 50.0,
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+            leading: Icon(Icons.arrow_forward_ios_outlined),
+            title: Text('Nombre de ruta'),
+            onTap: () {
+              //final route = MaterialPageRoute(builder: (context) => ListView1Screen(),);
+              Navigator.pushNamed(context, 'card');
+            },
+          ),
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: 5,
+      )
     );
   }
 }
