abe61ca33c67736abb23642f9c6be8a1c31381a5
daaviddieeguez <daaviddieeguez@gmail.com>
2025-10-31 09:03:06 +0100

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:fl_components/screens/alert_screen.dart';
+import 'package:fl_components/screens/card_screen.dart';
+import 'package:fl_components/screens/home_screen.dart';
+import 'package:fl_components/screens/listview1_screen.dart';
 import 'package:fl_components/screens/listview2_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +15,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,  
       title: 'Material App',
-      home:ListView2Screen()
+      home:ListView2Screen(),
+      routes: {
+        'listview1': (BuildContext context) => ListView1Screen(),
+        'listview2': (BuildContext context) => ListView2Screen(),
+        'card': (BuildContext context) => CardScreen(),
+        'alert': (BuildContext context) => AlertScreen(),
+        'home': (BuildContext context) => HomeScreen()
+      },
     );
   }
 }
